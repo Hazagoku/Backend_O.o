17faf5e053bb75d77909be719aa4592d57259307
RickJara <180300355@ucaribe.edu.mx>
2021-03-04 21:27:38 -0500

    Base de datos
    Esqueleto de backend con la base de datos.
    
    Co-Authored-By: Melgarejo-Alejandro <70671772+Melgarejo-Alejandro@users.noreply.github.com>

--- kavak_backend/cars/views.py
+++ kavak_backend/cars/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def hola_mundo(request):
-    return HttpResponse("Hola_mundos")
+    return HttpResponse("Hola_mundo")
