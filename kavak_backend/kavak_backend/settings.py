bd12b5f2637385b6f1d7001e319973cea2c729fe
Daniel Ventimilla <180300364@ucaribe.edu.mx>
2021-03-21 17:37:54 -0500

    create user
    view para crear un nuevo usuario

--- kavak_backend/kavak_backend/settings.py
+++ kavak_backend/kavak_backend/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'kavak_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates'),os.path.join(BASE_DIR,'test/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
