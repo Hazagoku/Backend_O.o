f795fb4b207fb73bf03eaf9179ee2fe13d334102
Daniel Ventimilla <180300364@ucaribe.edu.mx>
2021-05-22 15:41:21 -0500

    Filter Api
    filtro como API, donde recibe los campos a filtrar en la URL y devuelve los JSON de los autos filtrados (todos los JSON en caso de no dar filtro).

--- kavak_backend/cars/views.py
+++ kavak_backend/cars/views.py
@@ -20,32 +20,73 @@ def listing(request):
     page_obj = paginator.get_page(page_number)
     return render(request, 'pagination.html', {'page_obj': page_obj})
 
+def valido(param):
+    return param != '' and param is not None
+
 @api_view(["GET"])
 def car_list(request):
     datos = []
-    car = Car.objects.all()
-    tamaño = len(car)
-    page = request.GET.get('page', 1)
     
-    try:
-        paginator = Paginator(car,3)
-        car = paginator.page(page)
-    except:
-        raise Http404
+    page = request.GET.get('page', 1)
+    Carqs = Car.objects.all()
+    query_min_precio = request.GET.get('precio_min')
+    query_max_precio = request.GET.get('precio_max')
+    query_min_kilo = request.GET.get('kil_min')
+    query_max_kilo = request.GET.get('kil_max')
+    query_modelo = request.GET.getlist('modelo')
+    query_transmicion = request.GET.get('transmicion')
+    query_marca = request.GET.getlist('marca')
+    query_color = request.GET.getlist('color')
+    query_ciudad = request.GET.getlist('ciudad')
+    query_anio = request.GET.getlist('anio')
+    query_tipo = request.GET.getlist('tipo')
+    query_combustible = request.GET.getlist('combustible')
+    query_estatus = request.GET.getlist('status')
+    query_locacion = request.GET.getlist('locacion')
 
-    for x in car:
+    if(valido(query_min_precio)):
+        Carqs = Carqs.filter(price__gte=query_min_precio)
+    if(valido(query_max_precio)):
+        Carqs = Carqs.filter(price__lte=query_max_precio)
+    if((query_modelo)):
+        Carqs = Carqs.filter(carinfo_id__model__in =query_modelo)
+    if(valido(query_transmicion) and query_transmicion != 'Todas'):
+        Carqs = Carqs.filter(carinfo_id__transmission=query_transmicion)
+    if((query_marca)):
+        Carqs = Carqs.filter(carinfo_id__brand__in=query_marca)
+    if((query_color)):
+        Carqs = Carqs.filter(color__in=query_color)
+    if(valido(query_min_kilo)):
+        Carqs = Carqs.filter(km__gte=query_min_kilo)
+    if(valido(query_max_kilo)):
+        Carqs = Carqs.filter(km__lte=query_max_kilo)
+    if((query_ciudad)):
+        Carqs = Carqs.filter(city__in=query_ciudad)
+    if((query_anio)):
+        Carqs = Carqs.filter(year_purch__in=query_anio)
+    if((query_tipo)):
+        Carqs = Carqs.filter(carinfo_id__type__in=query_tipo)
+    if((query_combustible)):
+        Carqs = Carqs.filter(carinfo_id__fuel__in=query_combustible)
+    if((query_estatus)):
+        Carqs = Carqs.filter(status__in=query_estatus)
+    if((query_locacion)):
+        Carqs = Carqs.filter(location__in=query_locacion)
+
+    for x in Carqs:
         cii = Car_info.objects.get( id = x.carinfo_id_id )
         dt = {
             "car_id" : str(x.car_id),
-            "km" : str(x.km),
             "color" : str(x.color),
             "brand" :  str(cii.brand),
-            "num"   :  tamaño,
             "model" : str(cii.model),
             "year" : str(x.year_purch),
+            "location" : str(x.location),
             "price" : str(x.price),
-            "transmi" : cii.transmission,
-            "city" : x.city
+            "fuel" : str(cii.fuel),
+            "transmission" : str(cii.transmission),
+            "status" : str(x.status)
+
         }
         datos.append(dt)
     #return render(request, 'list.html', contexto)
