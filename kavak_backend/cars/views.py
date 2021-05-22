d71bda9c3979b0cbed1481326783469cacafa9c8
Melgarejo-Alejandro <70671772+Melgarejo-Alejandro@users.noreply.github.com>
2021-05-22 18:23:40 -0500

    Corrección de paginación
    Corrección de paginación

--- kavak_backend/cars/views.py
+++ kavak_backend/cars/views.py
@@ -15,7 +15,7 @@ import json
 
 def listing(request):
     car_list = Car.objects.all()
-    paginator = Paginator(car_list, 2) # Show 25 contacts per page.
+    paginator = Paginator(car_list, 3) # Show 25 contacts per page.
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'pagination.html', {'page_obj': page_obj})
@@ -26,73 +26,36 @@ def valido(param):
 @api_view(["GET"])
 def car_list(request):
     datos = []
-    
+    car = Car.objects.all()
+    tamaño = len(car)
     page = request.GET.get('page', 1)
-    Carqs = Car.objects.all()
-    query_min_precio = request.GET.get('precio_min')
-    query_max_precio = request.GET.get('precio_max')
-    query_min_kilo = request.GET.get('kil_min')
-    query_max_kilo = request.GET.get('kil_max')
-    query_modelo = request.GET.getlist('modelo')
-    query_transmicion = request.GET.get('transmicion')
-    query_marca = request.GET.getlist('marca')
-    query_color = request.GET.getlist('color')
-    query_ciudad = request.GET.getlist('ciudad')
-    query_anio = request.GET.getlist('anio')
-    query_tipo = request.GET.getlist('tipo')
-    query_combustible = request.GET.getlist('combustible')
-    query_estatus = request.GET.getlist('status')
-    query_locacion = request.GET.getlist('locacion')
 
-    if(valido(query_min_precio)):
-        Carqs = Carqs.filter(price__gte=query_min_precio)
-    if(valido(query_max_precio)):
-        Carqs = Carqs.filter(price__lte=query_max_precio)
-    if((query_modelo)):
-        Carqs = Carqs.filter(carinfo_id__model__in =query_modelo)
-    if(valido(query_transmicion) and query_transmicion != 'Todas'):
-        Carqs = Carqs.filter(carinfo_id__transmission=query_transmicion)
-    if((query_marca)):
-        Carqs = Carqs.filter(carinfo_id__brand__in=query_marca)
-    if((query_color)):
-        Carqs = Carqs.filter(color__in=query_color)
-    if(valido(query_min_kilo)):
-        Carqs = Carqs.filter(km__gte=query_min_kilo)
-    if(valido(query_max_kilo)):
-        Carqs = Carqs.filter(km__lte=query_max_kilo)
-    if((query_ciudad)):
-        Carqs = Carqs.filter(city__in=query_ciudad)
-    if((query_anio)):
-        Carqs = Carqs.filter(year_purch__in=query_anio)
-    if((query_tipo)):
-        Carqs = Carqs.filter(carinfo_id__type__in=query_tipo)
-    if((query_combustible)):
-        Carqs = Carqs.filter(carinfo_id__fuel__in=query_combustible)
-    if((query_estatus)):
-        Carqs = Carqs.filter(status__in=query_estatus)
-    if((query_locacion)):
-        Carqs = Carqs.filter(location__in=query_locacion)
+    try:
+        paginator = Paginator(car,3)
+        car = paginator.page(page)
+    except:
+        raise Http404
 
-    for x in Carqs:
+    for x in car:
         cii = Car_info.objects.get( id = x.carinfo_id_id )
         dt = {
             "car_id" : str(x.car_id),
+            "km" : str(x.km),
             "color" : str(x.color),
             "brand" :  str(cii.brand),
+            "num"   :  tamaño,
             "model" : str(cii.model),
             "year" : str(x.year_purch),
-            "location" : str(x.location),
             "price" : str(x.price),
-            "fuel" : str(cii.fuel),
-            "transmission" : str(cii.transmission),
-            "status" : str(x.status)
-
+            "transmi" : cii.transmission,
+            "city" : x.city
         }
         datos.append(dt)
     #return render(request, 'list.html', contexto)
     return JsonResponse(datos, safe = False)
     #http://127.0.0.1:8000/?page=1
 
+    
 @api_view(["POST"])
 def guardar(request):
     #Coloca los datos en la base de datos
